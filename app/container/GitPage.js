f1489747238d70bf1dc249a9b8f8124e85ce4ada
CHATHURA SAKUN ELLAWALA <sakun@villvay.com>
2019-01-10 20:23:10 +0530

    configure routes

---
+++ app/container/GitPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GitPage extends Component {
+
+    render = () => {
+        return (
+            null
+        );
+    }
+
+}
+
+export default GitPage;
---
+++ app/container/RedditPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reddit extends Component {
+
+    render = () => {
+        return (
+          null
+        );
+    }
+
+}
+
+export default Reddit;
